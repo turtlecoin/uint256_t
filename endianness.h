026b3d02eafab1de673ea89aaaa1585fce11a1bf
Brandon Lehmann <brandonlehmann@gmail.com>
2021-02-26 16:30:22 -0500

    Format endianness.h and fix the macro definitions so that they pick up the macros correctly

--- endianness.h
+++ endianness.h
@@ -1,24 +1,49 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2013 - 2017 Jason Lee @ calccrypto at gmail.com
+// Copyright (c) 2021, The TurtleCoin Developers
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 #ifndef _ENDIANNESS_H_
 #define _ENDIANNESS_H_
-#if defined(__BYTE_ORDER) && __BYTE_ORDER == __BIG_ENDIAN || \
-    defined(__BIG_ENDIAN__) ||                               \
-    defined(__ARMEB__) ||                                    \
-    defined(__THUMBEB__) ||                                  \
-    defined(__AARCH64EB__) ||                                \
-    defined(_MIBSEB) || defined(__MIBSEB) || defined(__MIBSEB__)
+
+#if (defined(__BYTE_ORDER__) && __BYTE_ORDER__ == __BIG_ENDIAN) ||             \
+    (defined(__BYTE_ORDER__) && __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__) ||     \
+    defined(__BIG_ENDIAN__) || defined(__ARMEB__) || defined(__THUMBEB__) ||   \
+    defined(__AARCH64EB__) || defined(_MIBSEB) || defined(__MIBSEB) ||         \
+    defined(__MIBSEB__)
 #ifndef __BIG_ENDIAN__
 #define __BIG_ENDIAN__
-#endif
-#elif defined(__BYTE_ORDER) && __BYTE_ORDER == __LITTLE_ENDIAN || \
-    defined(__LITTLE_ENDIAN__) ||                                 \
-    defined(__ARMEL__) ||                                         \
-    defined(__THUMBEL__) ||                                       \
-    defined(__AARCH64EL__) ||                                     \
-    defined(_MIPSEL) || defined(__MIPSEL) || defined(__MIPSEL__)
+#endif // __BIG_ENDIAN__
+
+#elif (defined(__BYTE_ORDER__) && __BYTE_ORDER__ == __LITTLE_ENDIAN) ||        \
+    (defined(__BYTE_ORDER__) && __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__) ||  \
+    defined(__LITTLE_ENDIAN__) || defined(__ARMEL__) ||                        \
+    defined(__THUMBEL__) || defined(__AARCH64EL__) || defined(_MIPSEL) ||      \
+    defined(__MIPSEL) || defined(__MIPSEL__)
 #ifndef __LITTLE_ENDIAN__
 #define __LITTLE_ENDIAN__
-#endif
-#else
+#endif // __LITTLE_ENDIAN
+
+#else // FALL_THROUGH
 #error "I don't know what architecture this is!"
-#endif
-#endif
+#endif // FALL_THROUGH
+
+#endif // _ENDIANNESS_H_
